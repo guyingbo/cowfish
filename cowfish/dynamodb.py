a425fd807fb7958c54ab0760f5b9535b1dc0b6b7
Gu Yingbo <guyingbo@papayamobile.com>
2017-09-30 11:05:14 +0800

    dynamodb put_item

--- cowfish/dynamodb.py
+++ cowfish/dynamodb.py
@@ -2,8 +2,8 @@ import aiobotocore
 from . import utils
 from cowfish.pool import Pool
 from boto3.dynamodb import types
-serializer = types.TypeSerializer()
-deserializer = types.TypeDeserializer()
+serialize = types.TypeSerializer().serialize
+deserialize = types.TypeDeserializer().deserialize
 
 
 class Dynamodb:
@@ -64,17 +64,16 @@ class Table:
 
     def _get_key(self, hash_key, range_key=None):
         Key = {}
-        Key[self.hash_key_name] = serializer.serialize(hash_key)
+        Key[self.hash_key_name] = serialize(hash_key)
         if range_key is not None:
-            Key[self.range_key_name] = serializer.serializer(range_key)
+            Key[self.range_key_name] = serialize(range_key)
         return Key
 
     async def get_item(self, hash_key, range_key=None):
         Key = self._get_key(hash_key, range_key)
         resp = await self.executor.get_item(TableName=self.name, Key=Key)
         if 'Item' in resp:
-            return {k: deserializer.deserialize(v)
-                    for k, v in resp['Item'].items()}
+            return {k: deserialize(v) for k, v in resp['Item'].items()}
 
     async def update_item(self, hash_key, range_key=None,
                           update_expression=None,
@@ -87,7 +86,32 @@ class Table:
             kw['UpdateExpression'] = update_expression
         if expression_attribute_values:
             kw['ExpressionAttributeValues'] = {
-                k: serializer.serialize(v)
-                for k, v in expression_attribute_values.items()
+                k: serialize(v) for k, v in expression_attribute_values.items()
             }
-        await self.executor.update_item(**kw)
+        resp = await self.executor.update_item(**kw)
+        if 'Item' in resp:
+            return {k: deserialize(v) for k, v in resp['Item'].items()}
+
+    async def put_item(self, hash_key,
+                       range_key=None,
+                       attributes=None,
+                       condition=None,
+                       expected=None,
+                       return_values=None,
+                       return_consumed_capacity=None,
+                       return_item_collection_metrics=None):
+        kw = {}
+        Item = self._get_key(hash_key, range_key)
+        if attributes:
+            Item.update({
+                k: serialize(v) for k, v in attributes.items()
+            })
+        kw['Item'] = Item
+        kw['ConditionalOperator'] = str(conditional_operator).upper()
+        kw['ReturnValues'] = str(return_values).upper()
+        kw['ReturnConsumedCapacity'] = str(return_consumed_capacity).upper()
+        kw['ReturnItemCollectionMetrics'] = \
+            str(return_item_collection_metrics).upper()
+        resp = await self.executor.put_item(**kw)
+        if 'Item' in resp:
+            return {k: deserialize(v) for k, v in resp['Item'].items()}
