b5c24324a069dee69f4b1ac19e3bca04289dacb7
Gu Yingbo <guyingbo@papayamobile.com>
2017-11-21 11:01:30 +0800

    original_batch method

--- cowfish/firehose.py
+++ cowfish/firehose.py
@@ -1,6 +1,7 @@
 import time
 import json
 import logging
+import asyncio
 import aiobotocore
 from .worker import BatchWorker
 logger = logging.getLogger(__name__)
@@ -8,10 +9,12 @@ logger = logging.getLogger(__name__)
 
 class Firehose:
     service_name = 'firehose'
+    MAX_RETRY = 10
 
     def __init__(self, stream_name, region_name,
                  encode_func=None, delimiter=b'\n',
-                 *, worker_params=None, client_params=None):
+                 *, worker_params=None, client_params=None,
+                 original_api=False):
         self.session = aiobotocore.get_session()
         self.stream_name = stream_name
         self.encode_func = encode_func or (lambda o: json.dumps(o).encode())
@@ -21,7 +24,8 @@ class Firehose:
         self.client = self.session.create_client(
             self.service_name, **client_params)
         worker_params = worker_params or {}
-        self.worker = BatchWorker(self.write_batch, **worker_params)
+        batch_func = self.original_batch if original_api else self.write_batch
+        self.worker = BatchWorker(batch_func, **worker_params)
 
     def __repr__(self):
         return '<{}: stream={}, worker={!r}>'.format(
@@ -52,3 +56,28 @@ class Firehose:
         except Exception as e:
             logger.exception(e)
             return obj_list
+
+    async def original_batch(self, obj_list, _seq=0):
+        if _seq > 0:
+            await asyncio.sleep(0.1 * (2 ** _seq))
+        if _seq > self.MAX_RETRY:
+            raise Exception(
+                'write_batch error: firehose put_record_batch failed')
+        try:
+            resp = await self.client.put_record_batch(
+                DeliveryStreamName=self.stream_name,
+                Records=[{
+                    'Data': self.encode_func(obj) + self.delimiter
+                } for obj in obj_list]
+            )
+        except Exception as e:
+            logger.exception(e)
+            return obj_list
+        if resp['FailedPutCount'] > 0:
+            failed_obj_list = [
+                obj_list[i] for i, record in
+                enumerate(resp['RequestResponses'])
+                if 'ErrorCode' in record]
+            return await self.write_batch(
+                failed_obj_list, _seq=_seq + 1
+            )
