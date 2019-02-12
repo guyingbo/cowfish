89b708fc1d26c4b521132146fc993dda68ea452d
Gu Yingbo <guyingbo@papayamobile.com>
2019-02-12 15:03:41 +0800

    mock test

--- tests/test_sqs.py
+++ tests/test_sqs.py
@@ -1,5 +1,6 @@
 import pytest
 import asyncio
+from unittest.mock import MagicMock
 from cowfish.sqs import SQSWriter
 
 
@@ -24,4 +25,32 @@ async def test_sqs(sqs_server):
     stop()
     for i in range(10):
         await writer.write_one({"test": 2}, queued=True)
+    await writer.stop()
+
+
+@pytest.mark.asyncio
+async def test_sqs2(sqs_server):
+    url, stop = sqs_server
+    client_params = {
+        "endpoint_url": url,
+        "aws_access_key_id": "xxx",
+        "aws_secret_access_key": "xxx",
+    }
+    writer = SQSWriter("fake", "us-east-1", client_params=client_params)
+    writer.sleep_base = 0.001
+    try:
+        await writer.client.create_queue(QueueName="fake")
+    except Exception as e:
+        print(e)
+    send_message_batch = MagicMock(
+        return_value={
+            "Failed": [
+                {"Id": "1", "SenderFault": False, "Code": 500, "Message": ""},
+                {"Id": "2", "SenderFault": True, "Code": 500, "Message": ""},
+            ]
+        }
+    )
+    writer.client.send_message_batch = send_message_batch
+    for i in range(10):
+        await writer.write_one({"test": 3}, queued=True)
     await writer.stop()
