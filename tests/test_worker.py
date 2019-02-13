ed6bed8bb5f88b1428ee88489b056cb61c2cf6a2
Gu Yingbo <guyingbo@papayamobile.com>
2019-02-13 10:42:06 +0800

    test

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -1,8 +1,17 @@
+import pytest
 import asyncio
 from cowfish.worker import BatchWorker
 
 
-async def go(worker):
+@pytest.mark.asyncio
+async def test_worker():
+    async def foo(objs):
+        if "bad" in objs:
+            raise Exception("bad")
+        await asyncio.sleep(0.3)
+        return objs[-1:] if len(objs) > 1 else []
+
+    worker = BatchWorker(foo, timeout=0.1)
     assert "BatchWorker" in repr(worker)
     assert worker.qsize == 0
     await worker.put("abc")
@@ -11,16 +20,3 @@ async def go(worker):
     await asyncio.sleep(0.2)
     await worker.put("bad")
     await worker.stop()
-
-
-def test_worker():
-    async def foo(objs):
-        if "bad" in objs:
-            raise Exception("bad")
-        await asyncio.sleep(0.3)
-        return objs[-1:] if len(objs) > 1 else []
-
-    event_loop = asyncio.get_event_loop()
-    worker = BatchWorker(foo, timeout=0.1)
-    event_loop.run_until_complete(go(worker))
-    event_loop.close()
